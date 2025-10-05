671c9ea4d8215b4de265238f6577414d344a9c21
HinoKami-Kagura1 <miguel.ramos9191@gmail.com>
2025-10-05 00:49:51 -0500

    PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+
+    public float speed = 5;
     private int facingDirection = 1;
 
     public Rigidbody2D rb;
     public Animator anim;
 
+
+    // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
 
-        if (horizontal > 0 && facingDirection < 0 ||
-            horizontal < 0 && facingDirection > 0)
+        float Horizontal = Input.GetAxis("Horizontal");
+        //float Vertical = Input.GetAxis("Vertical");
+        
+        if (Horizontal > 0 && transform.localScale.x < 0 ||
+            Horizontal < 0 && transform.localScale.x > 0)
         {
             Flip();
         }
 
-        anim.SetFloat("Speed", Mathf.Abs(horizontal));
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        anim.SetFloat("Horizontal", Mathf.Abs(Horizontal));
+        //anim.SetFloat("Vertical", Mathf.Abs(Vertical));
+
+
+        rb.linearVelocity = new Vector2(Horizontal * speed, rb.linearVelocity.y);
+
+
     }
+
 
     void Flip()
     {
         facingDirection *= -1;
-        Vector3 scale = transform.localScale;
-        scale.x = facingDirection;
-        transform.localScale = scale;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
+    
+
 }
